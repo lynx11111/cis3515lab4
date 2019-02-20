85de44aea2151a1e57a6056281fd3ebb023a76d8
lynx11111 <46969236+lynx11111@users.noreply.github.com>
2019-02-19 21:55:50 -0500

    Add files via upload

---
+++ java/edu/temple/cis3515lab4/CanvasActivity.java
@@ -0,0 +1,18 @@
+package edu.temple.cis3515lab4;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+
+public class CanvasActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_canvas);
+
+        ConstraintLayout constraintLayout = findViewById(R.id.constraintLayout);
+        constraintLayout.setBackgroundColor(Color.parseColor(getIntent().getStringExtra("color")));
+    }
+}
---
+++ java/edu/temple/cis3515lab4/PaletteActivity.java
@@ -0,0 +1,41 @@
+package edu.temple.cis3515lab4;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+
+public class PaletteActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_palette);
+
+        Resources res = getResources();
+        final String[] colors = res.getStringArray(R.array.Colors);
+
+
+        ColorAdapter colorAdapter = new ColorAdapter(this, colors);
+
+
+        GridView gridView = findViewById(R.id.gridView);
+        gridView.setAdapter(colorAdapter);
+
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Intent intent = new Intent(PaletteActivity.this, CanvasActivity.class);
+
+                intent.putExtra("color", colors[position]);
+
+                startActivity(intent);
+            }
+        });
+    }
+}
